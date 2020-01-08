61e5e5b8184a69cdcf3d455cb71341bf71d31f88
Tanapon Kongjaroensuk <tanapon395@gmail.com>
2020-01-08 18:41:53 +0700

    reply message

--- index.php
+++ index.php
@@ -1,3 +1,60 @@
 <?php
-  http_response_code(200);
+    /*Get Data From POST Http Request*/
+    $datas = file_get_contents('php://input');
+    /*Decode Json From LINE Data Body*/
+    $deCode = json_decode($datas,true);
+    file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+    $replyToken = $deCode['events'][0]['replyToken'];
+    $messages = [];
+    $messages['replyToken'] = $replyToken;
+    $messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
+    $encodeJson = json_encode($messages);
+    $LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+      $LINEDatas['token'] = "hbKDhprLVFGyn4RGGuZ1LXmerp3J7/jMZv8NNJgDq9Vsoxzz37siTfaR6l0biG2HvugTbKX8GjNHYbZ2bHRMnyE5JMm+2iX3fpqhWQ0ntmVFnJ5UDKsltPawAMysPdfHBb4Zm8WuiQcjvegV6jIJ+AdB04t89/1O/w1cDnyilFU=";
+      $results = sentMessage($encodeJson,$LINEDatas);
+    /*Return HTTP Request 200*/
+    http_response_code(200);
+    function getFormatTextMessage($text)
+    {
+        $datas = [];
+        $datas['type'] = 'text';
+        $datas['text'] = $text;
+        return $datas;
+    }
+    function sentMessage($encodeJson,$datas)
+    {
+        $datasReturn = [];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $datas['url'],
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => $encodeJson,
+          CURLOPT_HTTPHEADER => array(
+            "authorization: Bearer ".$datas['token'],
+            "cache-control: no-cache",
+            "content-type: application/json; charset=UTF-8",
+          ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            $datasReturn['result'] = 'E';
+            $datasReturn['message'] = $err;
+        } else {
+            if($response == "{}"){
+            $datasReturn['result'] = 'S';
+            $datasReturn['message'] = 'Success';
+            }else{
+            $datasReturn['result'] = 'E';
+            $datasReturn['message'] = $response;
+            }
+        }
+        return $datasReturn;
+    }
 ?>
